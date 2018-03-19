96b6de1b0fec136fa9815d9b395e788b56221639
TailoMateus <tailogonsalves@gmail.com>
2018-03-19 17:48:18 -0300

    Add project

---
+++ README.md
@@ -0,0 +1,3 @@
+#### Testing React router dom
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <Link to="/sobre">Ir para a página sobre</Link>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
